985caaffe91894f4639b4e95d99074e045d83a69
dd-712 <divya807950@gmail.com>
2022-10-25 16:30:34 +0530

    user profile created

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
 #     blogs
     path("", views.blogs, name="blogs"),
 
+#     profile
+    path("profile/", views.profile_view, name="profile"),
+    path("edit_profile/", views.edit_profile, name="edit_profile"),
+    path("user_profile/<int:myid>/", views.user_profile, name="user_profile"),
+    
 #    user authentication
     path("register/", views.Register, name="register"),
     path("login/", views.Login, name="login"),
--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,29 @@ from django.views.generic import UpdateView
 from django.contrib import messages
 
 def blogs(request):
-    return render("blog.html")
+    return render(request,"blog.html")
+
+def user_profile(request, myid):
+    # post = BlogPost.objects.filter(id=myid)
+    return render(request, "profile.html")
+
+def profile_view(request):
+    return render(request, "profile.html")
+
+def edit_profile(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = Profile(user=request.user)
+    if request.method=="POST":
+        form = ProfileForm(data=request.POST, files=request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            alert = True
+            return render(request, "edit_profile.html", {'alert':alert})
+    else:
+        form=ProfileForm(instance=profile)
+    return render(request, "edit_profile.html", {'form':form})
 
 def Register(request):
     if request.method=="POST":   
@@ -23,6 +45,13 @@ def Register(request):
             messages.error(request, "Passwords do not match.")
             return redirect('/register')
         
+        alert = {
+            "username": request.GET.get('username', ''),
+        }
+        if User.objects.filter(username = request.POST['username']).exists():
+            alert['username'] = "Username already exists"
+            return render(request, "register.html", alert)
+
         user = User.objects.create_user(username, email, password1)
         user.first_name = first_name
         user.last_name = last_name
@@ -43,7 +72,7 @@ def Login(request):
             return redirect("/")
         else:
             messages.error(request, "Invalid Credentials")
-        return render(request, 'blog.html')   
+        return render(request, 'login.html')   
     return render(request, "login.html")
 
 def Logout(request):
