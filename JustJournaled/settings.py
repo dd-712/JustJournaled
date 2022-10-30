e0d6d33ea46d3e7e9c6bfbe895b4ef81857d725f
dd-712 <divya807950@gmail.com>
2022-10-30 18:54:19 +0530

    styles updated

--- JustJournaled/settings.py
+++ JustJournaled/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "blog", "static")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEBUG= True
+
+mimetypes.add_type("image/svg+xml", ".svg", True)
+mimetypes.add_type("image/svg+xml", ".svgz", True)
